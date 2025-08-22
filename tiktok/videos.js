722f9c1d501e8653fc88cd56bf9b572066ffae5a
allshoez <allshoez88@gmail.com>
2025-08-22 16:08:39 +0700

    Add file(s) 2025-08-22 16:08:39

---
+++ tiktok/videos.js
@@ -0,0 +1,152 @@
+const allVideos = {
+  "Cinema": [
+    {
+      "src": "videos/Cinema/cutties.mp4",
+      "title": "cutties"
+    }
+  ],
+  "Comedi": [
+    {
+      "src": "videos/Comedi/Bola.mp4",
+      "title": "Bola"
+    }
+  ],
+  "Funny": [
+    {
+      "src": "videos/Funny/Hantu.mp4",
+      "title": "Hantu"
+    },
+    {
+      "src": "videos/Funny/Lipstik.mp4",
+      "title": "Lipstik"
+    },
+    {
+      "src": "videos/Funny/Solek.mp4",
+      "title": "Solek"
+    }
+  ],
+  "Kids": [
+    {
+      "src": "videos/Kids/Animation.mp4",
+      "title": "Animation"
+    },
+    {
+      "src": "videos/Kids/Ayam.mp4",
+      "title": "Ayam"
+    },
+    {
+      "src": "videos/Kids/Cat duck.mp4",
+      "title": "Cat duck"
+    },
+    {
+      "src": "videos/Kids/Doa ortu.mp4",
+      "title": "Doa ortu"
+    },
+    {
+      "src": "videos/Kids/Gambar Lebah.mp4",
+      "title": "Gambar Lebah"
+    },
+    {
+      "src": "videos/Kids/Gambar Panda.mp4",
+      "title": "Gambar Panda"
+    },
+    {
+      "src": "videos/Kids/Gambar bebek.mp4",
+      "title": "Gambar bebek"
+    },
+    {
+      "src": "videos/Kids/Gambar boneka.mp4",
+      "title": "Gambar boneka"
+    },
+    {
+      "src": "videos/Kids/Gambar kelinci.mp4",
+      "title": "Gambar kelinci"
+    },
+    {
+      "src": "videos/Kids/Ikan.mp4",
+      "title": "Ikan"
+    },
+    {
+      "src": "videos/Kids/Jebak tikus kucing.mp4",
+      "title": "Jebak tikus kucing"
+    },
+    {
+      "src": "videos/Kids/Lukis berby.mp4",
+      "title": "Lukis berby"
+    },
+    {
+      "src": "videos/Kids/Menggambar.mp4",
+      "title": "Menggambar"
+    },
+    {
+      "src": "videos/Kids/Monkey funny.mp4",
+      "title": "Monkey funny"
+    },
+    {
+      "src": "videos/Kids/Monyet makan buah.mp4",
+      "title": "Monyet makan buah"
+    },
+    {
+      "src": "videos/Kids/Mulikis kanggusru.mp4",
+      "title": "Mulikis kanggusru"
+    },
+    {
+      "src": "videos/Kids/Panda.mp4",
+      "title": "Panda"
+    },
+    {
+      "src": "videos/Kids/Sapi anak.mp4",
+      "title": "Sapi anak"
+    }
+  ],
+  "Music": [
+    {
+      "src": "videos/Music/ABCDE.mp4",
+      "title": "ABCDE"
+    },
+    {
+      "src": "videos/Music/Alif Ba Ta.mp4",
+      "title": "Alif Ba Ta"
+    },
+    {
+      "src": "videos/Music/Ayam warna warni.mp4",
+      "title": "Ayam warna warni"
+    },
+    {
+      "src": "videos/Music/Balon ku ada 5.mp4",
+      "title": "Balon ku ada 5"
+    },
+    {
+      "src": "videos/Music/Balon metus monyet.mp4",
+      "title": "Balon metus monyet"
+    },
+    {
+      "src": "videos/Music/Cicak di dinding.mp4",
+      "title": "Cicak di dinding"
+    },
+    {
+      "src": "videos/Music/Cit cit.mp4",
+      "title": "Cit cit"
+    },
+    {
+      "src": "videos/Music/Kucing ApaTa.mp4",
+      "title": "Kucing ApaTa"
+    },
+    {
+      "src": "videos/Music/Kucing jogef.mp4",
+      "title": "Kucing jogef"
+    },
+    {
+      "src": "videos/Music/Labubu.mp4",
+      "title": "Labubu"
+    },
+    {
+      "src": "videos/Music/Monyet balon ada 5.mp4",
+      "title": "Monyet balon ada 5"
+    },
+    {
+      "src": "videos/Music/Musik Dj.mp4",
+      "title": "Musik Dj"
+    }
+  ]
+};
